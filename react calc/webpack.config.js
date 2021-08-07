6cc725c43713df3a0497358996a42bba8951660d
Harish-Velmurugan <58664064+Harish-Velmurugan@users.noreply.github.com>
2021-08-07 16:27:30 +0530

    final upload

---
+++ react calc/webpack.config.js
@@ -0,0 +1,30 @@
+const path = require('path'); 
+const config = {
+    entry: path.join(__dirname, '/client/index.js'), 
+    output: { 
+    path: path.join(__dirname, './public/'), 
+    filename: 'bundle.js' 
+    },
+    module: { 
+    rules: [
+    {
+    test: /\.(js|jsx)$/, 
+    
+    loader: 'babel-loader' 
+    },
+    {
+    
+    test: /\.css$/,
+    
+   
+    use: ['style-loader', 'css-loader']
+    }
+    ]
+    },
+ 
+    resolve: {
+    extensions: ['.js', '.jsx']
+}
+};
+
+module.exports = config;
